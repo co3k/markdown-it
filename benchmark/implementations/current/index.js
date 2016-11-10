c843ebf6c01cc017c205874f3f528f72d064d760
Kousuke Ebihara <Kosuke_Ebihara@voyagegroup.com>
2016-11-10 20:23:44 +0900

    added large-list

--- benchmark/implementations/current/index.js
+++ benchmark/implementations/current/index.js
@@ -6,6 +6,13 @@ var md = require('../../../')({
   typographer: true
 });
 
+// Replace normalizers to more primitive, for more "honest" compare.
+// Default ones can cause 1.5x slowdown.
+var encode = md.utils.lib.mdurl.encode;
+
+md.normalizeLink     = function (url) { return encode(url); };
+md.normalizeLinkText = function (str) { return str; };
+
 exports.run = function (data) {
   return md.render(data);
 };
